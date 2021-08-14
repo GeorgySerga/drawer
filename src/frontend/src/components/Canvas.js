a5ebae55ddde4507026e5ebdad66d4401aa7a6b0
Georgy Serga <georgy.serga@gmail.com>
2021-08-13 18:51:02 -0700

    Allow drawing with a basic brush on canvas

--- src/frontend/src/components/Canvas.js
+++ src/frontend/src/components/Canvas.js
@@ -1,7 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 const Canvas = () => {
+  const [isDrawing, setIsDrawing] = useState(false);
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -9,9 +11,45 @@ const Canvas = () => {
 
     context.fillStyle = 'blue';
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.lineWidth = 5;
+
+    contextRef.current = context;
   }, []);
 
-  return <canvas ref={canvasRef} width="400px" height="300px" />;
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      width="400px"
+      height="300px"
+    />
+  );
 };
 
 export default Canvas;
