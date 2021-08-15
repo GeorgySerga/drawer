8e142ade159dc46edb1dc54167e245bbd3ff1bbf
Georgy Serga <georgy.serga@gmail.com>
2021-08-14 19:34:30 -0700

    Add basic image loading uploading in FE

--- src/frontend/src/components/Canvas/index.js
+++ src/frontend/src/components/Canvas/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import useCanvas from '../../hooks/useCanvas';
 import CanvasControls from './controls';
 
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  gridItem: {
+    marginBottom: '10px',
+  },
+});
+
 const Canvas = () => {
   const { Canvas, context, save } = useCanvas();
+  const classes = useStyles();
+
+  function onClick() {
+    fetch('http://localhost:3001/images', {
+      method: 'POST',
+      body: JSON.stringify({
+        private: false,
+        user: 'test',
+        image: context.current.canvas.toDataURL(),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
   return (
     <Grid
@@ -13,11 +37,16 @@ const Canvas = () => {
       justifyContent="center"
       alignItems="center"
     >
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} className={classes.gridItem}>
         {Canvas}
       </Grid>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} className={classes.gridItem}>
         <CanvasControls context={context} save={save} />
+      </Grid>
+      <Grid item xs={12} md={6} className={classes.gridItem}>
+        <Button variant="contained" color="primary" onClick={onClick}>
+          Upload
+        </Button>
       </Grid>
     </Grid>
   );
--- src/frontend/src/components/TitlebarImageList.js
+++ src/frontend/src/components/TitlebarImageList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
@@ -7,7 +7,6 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import Paper from '@material-ui/core/Paper';
-import image from './logo512.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,58 +27,23 @@ const useStyles = makeStyles((theme) => ({
   },
   paperImageContainer: {
     padding: '10px',
+    [theme.breakpoints.up('sm')]: {
+      minWidth: '380px',
+    },
   },
 }));
 
-const itemData = [
-  {
-    img: image,
-    title: 'Image',
-    author: 'author',
-  },
-  {
-    img: image,
-    title: 'Image1',
-    author: 'author1',
-  },
-  {
-    img: image,
-    title: 'Image2',
-    author: 'author2',
-  },
-  {
-    img: image,
-    title: 'Image3',
-    author: 'author3',
-  },
-  {
-    img: image,
-    title: 'Image4',
-    author: 'author4',
-  },
-  {
-    img: image,
-    title: 'Image5',
-    author: 'author5',
-  },
-  {
-    img: image,
-    title: 'Image6',
-    author: 'author6',
-  },
-  {
-    img: image,
-    title: 'Image7',
-    author: 'author7',
-  },
-  {
-    img: image,
-    title: 'Image8',
-    author: 'author8',
-  },
-];
 function TitlebarImageList() {
   const classes = useStyles();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('/images');
+      const json = await data.json();
+      setData(json);
+    })();
+  }, []);
 
   return (
     <div className={classes.root}>
@@ -88,15 +52,14 @@ function TitlebarImageList() {
           <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
             <ListSubheader component="div">Public drawings</ListSubheader>
           </ImageListItem>
-          {itemData.map((item) => (
-            <ImageListItem key={item.img + item.title}>
-              <img src={item.img} alt={item.title} />
+          {data.map((item) => (
+            <ImageListItem key={item.image}>
+              <img src={item.image} alt="Public drawing" />
               <ImageListItemBar
-                title={item.title}
-                subtitle={<span>by: {item.author}</span>}
+                subtitle={<span>by: {item.username}</span>}
                 actionIcon={
                   <IconButton
-                    aria-label={`info about ${item.title}`}
+                    aria-label={`info about ${item.username}'s work`}
                     className={classes.icon}
                   >
                     <InfoIcon />
