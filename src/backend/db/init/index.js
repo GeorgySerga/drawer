85481c986ea8308d014e095693873deafcdf066a
Georgy Serga <georgy.serga@gmail.com>
2021-08-17 20:46:12 -0700

    Update init scripts with necessary data and tables

--- src/backend/db/init/index.js
+++ src/backend/db/init/index.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+const { image1, image2, image3 } = require('./images');
 
 const client = new Client({
   user: 'postgres',
@@ -23,6 +24,10 @@ const execute = async (queries) => {
   }
 };
 
+const dropDrawings = `drop table if exists drawings;`;
+const dropUsers = `drop table if exists users;`;
+const dropSession = `drop table if exists session;`;
+
 const drawings = `
     CREATE TABLE IF NOT EXISTS "drawings" (
 	    "id" SERIAL,
@@ -41,8 +46,39 @@ const users = `
 	    PRIMARY KEY ("id")
     );`;
 
-execute([drawings, users]).then((result) => {
+// password is 123
+const createUser1 = `INSERT INTO users (username, password) VALUES ('user1', '$2b$10$lK.2uISnM4FvySl8R0kqEu5GQ/Y4wqRF4cgRkLBimFzDqLx4eTrRO')`;
+
+const createDrawing1 = `INSERT INTO drawings (image, private, username) VALUES ('${image1}', false, 'user1')`;
+const createDrawing2 = `INSERT INTO drawings (image, private, username) VALUES ('${image2}', false, 'user1')`;
+const createDrawing3 = `INSERT INTO drawings (image, private, username) VALUES ('${image3}', false, 'user1')`;
+
+// Taken from https://github.com/voxpelli/node-connect-pg-simple/blob/HEAD/table.sql
+const session = `
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");`;
+
+execute([
+  dropDrawings,
+  dropUsers,
+  dropSession,
+  drawings,
+  users,
+  session,
+  createUser1,
+  createDrawing1,
+  createDrawing2,
+  createDrawing3,
+]).then((result) => {
   if (result) {
-    console.log('Tables created');
+    console.log('Tables created, data is seeded');
   }
 });
