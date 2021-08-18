ae9d34aa8ab251097067f5c40e7b7a0a47b5b14e
Georgy Serga <georgy.serga@gmail.com>
2021-08-17 18:43:59 -0700

    Remove localstorage image data on logout

--- src/frontend/src/components/Logout.js
+++ src/frontend/src/components/Logout.js
@@ -6,8 +6,15 @@ import { useAuthContext } from '../providers/AuthProvider';
 const Logout = () => {
   const { setUser } = useAuthContext();
   const logout = () => {
+    /**
+     * Reset session on the react state.
+     * Remove session from local storage.
+     * Removed saved image from localstorage.
+     * Invalidate the session.
+     */
     setUser(null);
     localStorage.setItem('user', JSON.stringify(null));
+    localStorage.removeItem('drawingHistory');
     fetch('/api/logout');
   };
   return (
